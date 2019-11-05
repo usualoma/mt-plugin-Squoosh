545f103288f38fb3e8e5249906160e4c9bb6356f
Taku AMANO <taku@taaas.jp>
2019-11-06 08:56:33 +0900

    Squoosh app.

---
+++ components/App/client-api.d.ts
@@ -0,0 +1,29 @@
+import App from './index';
+export interface ReadyMessage {
+    type: 'READY';
+    version: string;
+}
+export declare function exposeAPI(app: App): void;
+/**
+ * The API class contains the methods that are exposed via Comlink to the
+ * outside world.
+ */
+export declare class API {
+    private _app;
+    /**
+     * Internal constructor. Do not call.
+     */
+    constructor(_app: App);
+    /**
+     * Loads a given file into Squoosh.
+     * @param blob The `Blob` to load
+     * @param name The name of the file. The extension of this name will be used
+     * to deterime which decoder to use.
+     */
+    setFile(blob: Blob, name: string): Promise<unknown>;
+    /**
+     * Grabs one side from Squoosh as a `File`.
+     * @param side The side which to grab. 0 = left, 1 = right.
+     */
+    getBlob(side: 0 | 1): Promise<unknown>;
+}
